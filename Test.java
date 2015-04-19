9224a660512cbc5e9700e014d819a680c4635ddc
AM021705 <km75557ca@gmail.com>
2015-04-19 21:45:13 +0800

    AM021705

--- Test.java
+++ Test.java
@@ -5,7 +5,11 @@ public class Test
 	public static void main (String [] args)
 	{
 		System.out.println("資工二A	AM021698	朱泰瑞");
-		System.out.println("資工二A	AM021705	魏俊騏 改1");
 		System.out.println("資工二A	AM021698	朱泰瑞");
+		System.out.println("資工二A	AM021698	朱泰瑞");
+
+                System.out.println("資工二A	AM021705	魏俊騏");
+		System.out.println("資工二A	AM021705	魏俊騏");
+		System.out.println("資工二A	AM021705	魏俊騏");
 	}
 }
