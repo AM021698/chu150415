11f08b3cee223636fe58add242b23f8c6637cbc9
unknown <chu_tae@hotmail.com.tw>
2015-04-21 13:53:56 +0800

    AM021698

---
+++ Chu.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Chu
+{
+	public static void main (String [] args)
+	{
+		Scanner input=new Scanner (System.in);
+		int x;
+		int y;
+		int sum;
+
+		x=input.nextInt();
+		y=input.nextInt();
+		sum=x+y;
+
+		System.out.println("%d+%d=%d",x,y,sum);
+
+	}
+}
